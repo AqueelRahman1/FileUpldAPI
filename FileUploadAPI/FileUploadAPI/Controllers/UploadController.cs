2abb656215f20e5e72b9168edb4b803c363d83c6
AqueelRahman1 <39337888+AqueelRahman1@users.noreply.github.com>
2021-07-03 14:36:06 +0530

    Initial commit

---
+++ FileUploadAPI/FileUploadAPI/Controllers/UploadController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUploadAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpGet("GetFileData/{id}")]
+        public ActionResult<string> GetFileData(int id)
+        {
+            return "File with ID: " + id + ", is here:"; 
+        }
+
+        [HttpPost("SetConfigValues")]
+        public void SetConfigValues()
+        {
+
+        }
+
+        [HttpPost("UploadFile")]
+        public async Task<ActionResult> UploadFileAsync([FromForm]CreateFile model)
+        {
+            try
+            {
+                if (model.File == null || model.File.Length == 0)
+                {
+                    return Content("File not Selected");
+                }
+
+                /*Code to upload files to a path*/
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", model.File.FileName);
+                //Path should be set via Cofig Keys
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await model.File.CopyToAsync(stream);
+                }
+                /*File upload code ends*/
+                     
+                /*Code to store meta data in DB*/
+
+
+                /*DB code ends*/
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal Server Error: {ex}");
+            }            
+        }
+
+        //
+        //
+    }
+}
---
+++ FileUploadAPI/FileUploadAPI/Models/CreateFile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadAPI.Models
+{
+    public class CreateFile
+    {
+        public string FileAltText { get; set; }
+
+        public string FileName { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
